f7c9f7fccbe324798a4ec7f720f7b082eefcc762
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-01-31 11:30:59 +0000

    added previous and next buttons

--- slide.js
+++ slide.js
@@ -3,6 +3,7 @@
 var arr = ['A', 'B', 'C', 'D', 'E'];
 console.log(arr);
 
+/*
 for (let i = 0; i < 5; i++){ 
 	let Selected = arr.shift();
 	console.log(Selected);
@@ -16,5 +17,13 @@ for (let i = 0; i < 5; i++){
 		console.log(i); 
 	}
 }
+ */
+
+let selectedPrev = arr[arr.length-1];
+console.log(selectedPrev);
+
+arr.pop(selectedPrev);
+arr.unshift(selectedPrev);
+console.log(arr, arr[0]);
 
 
